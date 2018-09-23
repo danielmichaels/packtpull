d962755c260bb9a8bdcd877409e2224b91bd831e
Dan <dans.address@outlook.com>
2018-09-23 12:13:53 +0000

    Email function added
    notes:
    + program will email to recipient if criteria met with link to login.

--- program.py
+++ program.py
@@ -4,8 +4,13 @@ Python script to check what books are on offer for packt today. It sends the
 link to download if it meets criteria.
 """
 import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from requests_html import HTMLSession
+
+from env import envar
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -21,6 +26,9 @@ class Packtpull:
     def __init__(self):
         self.url = 'https://www.packtpub.com//packt/offers/free-learning'
         self.matches = ['python', 'go', 'golang', 'devops']
+        self.email_account = envar.EMAIL_USERNAME
+        self.email_password = envar.EMAIL_PASSWORD
+        self.recipient = envar.RECIPIENT
 
     def check_title(self):
         session = HTMLSession()
@@ -34,8 +42,27 @@ class Packtpull:
         logging.info(f" {title} Currently Available.")
         for word in words:
             if word in self.matches:
-                # logic here that alerts if in list.
-                print(f'{word} found!')
+                self.send_email(title)
+
+    def send_email(self, title):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.set_debuglevel(1)
+        server.starttls()
+        server.login(self.email_account, self.email_password)
+        msg = MIMEMultipart()
+        msg['From'] = self.email_account
+        msg['To'] = self.recipient
+        msg['Subject'] = title
+        text = f"""
+        <body>
+        <h1> {title} </h1>
+        <p>Link to sign in and download eBook</p>
+        <a href='{self.url}'>Click Here</a>
+        </body>
+        """
+        msg.attach(MIMEText(text, 'html'))
+        server.sendmail(self.email_account, self.recipient, msg.as_string())
+        server.quit()
 
     def run(self):
         return self.worth_keeping()
